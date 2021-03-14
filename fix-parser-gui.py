80ef1e556dd7771f4d80559f91489041dd0f2b76
Diwash Rai <diwash.rai1993@gmail.com>
2021-03-14 19:28:22 +0000

    refreshes output_frame when parsing now. Tweaked enum logic. Few other things I forgot

--- fix-parser-gui.py
+++ fix-parser-gui.py
@@ -30,7 +30,7 @@ class ParseFIX(tk.Frame):
         fix_string_entry = tk.Entry(master=self.entry_frame, width=100, textvariable=self.fix_string)
         fix_string_entry.grid(row=2, column=1)
         parse_btn = tk.Button(master=self.entry_frame, text="PARSE", command=self.parse)
-        parse_btn.grid(row=3, column=0)
+        parse_btn.grid(row=7, column=0)
 
         self.output_canvas = tk.Canvas(master=self.mainframe, highlightthickness=0)
         self.output_frame = tk.Frame(master=self.output_canvas, padx=5, pady=5, relief=tk.SOLID, borderwidth=1)
@@ -41,8 +41,6 @@ class ParseFIX(tk.Frame):
         self.output_canvas.pack(side="left", fill="both", expand=True)
         self.output_canvas.create_window((4,4), window=self.output_frame, anchor="nw", tags="self.output_frame")
         self.output_frame.bind("<Configure>", self.onFrameConfigure)
-
-        self.fix_string.set("8=FIX.4.4|9=954|35=AE|34=470|49=CMEGROUP|50=STP|52=20200708-21:26:50.297|56=GFE_GFEU1|57=URU27|571=1732375C2B41018D15D462404335766459502|487=0|856=101|568=3|828=0|820=140008662|880=4219245|17=4290:M:1187TN0028998|423=9|55=U07-25:R4TWM6TTV63R|48=U07-25|22=H|454=1|455=9128286F2|456=1|167=REPO|762=SPEC|207=BTUS|107=2_1/2_02/26 REG|916=20200709|917=20200710|711=1|311=[N/A]|810=111.82421875|882=112.625|32=1000|31=1.000000000|75=20200708|715=20200708|442=1|60=20200708-21:07:05.570212592Z|64=20200709|552=1|54=2|37=42202810|11=234|453=5|448=GFEU2|447=D|452=1|802=2|523=AUTO CERT TEST FIRM GFEU2|803=5|523=G8ZTNESVNKW4NN761W05|803=84|448=gfe_btec_qai_us2|447=C|452=7|448=FICC|447=C|452=21|448=URU22|452=44|448=EED|452=55|578=GLBX|921=1126250000.00|922=1126281284.72|1016=1|1012=20200708-21:07:05.570212592Z|1013=1|1057=N|779=20200708-21:07:05.671000000Z|1003=260819|1040=1732375C2B41018D15D48|939=0|1430=E|10026=USD|1832=1|37513=644707884226771825|10053=N|20056=R0000260819|20011=101124|2490=28998|10=176|")
 
 
     def onFrameConfigure(self, event):
@@ -63,10 +61,20 @@ class ParseFIX(tk.Frame):
 
 
     def parse(self):
+        for widget in self.output_frame.winfo_children():
+            widget.destroy()
         fix_str = self.fix_string.get()
         tags_list = re.split("\||\^|", fix_str)
-        for i in range(len(tags_list)):
-            pair = tags_list[i].split("=")
+        header1 = tk.Label(master=self.output_frame, height=1, text="Tag")
+        header2 = tk.Label(master=self.output_frame, height=1, text="Tag Name")
+        header3 = tk.Label(master=self.output_frame, height=1, text="Value")
+        header4 = tk.Label(master=self.output_frame, height=1, text="Enums")
+        header1.grid(row=0, column=0)
+        header2.grid(row=0, column=1)
+        header3.grid(row=0, column=2)
+        header4.grid(row=0, column=3)
+        for i in range(1, len(tags_list) + 1):
+            pair = tags_list[i - 1].split("=")
             if len(pair) == 1:
                 continue
 
@@ -87,18 +95,25 @@ class ParseFIX(tk.Frame):
             tag_value_text.grid(row=i, column=2)
 
             field = self.fields.find(f"./field[@number='{tag_number}']")
-            firstVal = field.find("./value")
-            if not firstVal is None:
+            enum_arr = []
+            list_pos = 0
+            for enum in field:
+                if enum.get("enum") == tag_value:
+                    list_pos = len(enum_arr)
+                enum_arr.append(enum.get("enum") + " : " + enum.get("description"))
+
+            if enum_arr:
+                combo = ttk.Combobox(master=self.output_frame, values=enum_arr, state="readonly")
+                combo.grid(row=i, column=3)
                 meaning = field.find(f"./value[@enum='{tag_value}']")
-                if not meaning is None:
-                    enum = " (" + meaning.get("description") + ")"
-                    combo = ttk.Combobox(master=self.output_frame, values=[tag_value + " : " + enum], state="readonly")
-                    combo.current(0)
-                    combo.grid(row=i, column=3)
+                enum = " (" + meaning.get("description") + ")"
+                combo.current(list_pos)
 
             tag_number_text.configure(state="disabled")
             tag_name_text.configure(state="disabled")
             tag_value_text.configure(state="disabled")
+
+        self.root.update_idletasks()
 
 
 if __name__ == "__main__":
